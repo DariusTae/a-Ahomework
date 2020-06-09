70db84ad2165d4124d650b52571042d522f18f70
DariusFreeman <DariusTae@gmail.com>
2020-06-09 12:12:04 -0500

    Why is this working?

---
+++ getDomed/myProfile.js
@@ -0,0 +1,48 @@
+ window.addEventListener("DOMContentLoaded", event => {
+//     let daRi = document.createElement("h1");
+//     daRi.setAttribute("id", "daRi");
+//     let ius = document.createTextNode("Darius is pretty bad at things");
+//     daRi.appendChild(ius);
+//     document.body.appendChild(daRi);
+
+//     let unOrdered = document.createElement("ul");
+//     unOrdered.setAttribute("id", "unOrdered", "class", "details");
+//     document.body.appendChild(unOrdered);
+//     let listed = [
+//         "<li>I have loved and been loved.</li>",
+//         "<li>There is no greater thing than </li>",
+//         "<li>to love And be loved in return</li>",
+//         "<li>I really can do this.</li>"
+//     ]
+//     const liste = listed.join(" ");
+//     const elemntList = document.getElementById("unOrdered");
+//     elemntList.innerHTML = liste;
+// })
+// window.onload = (event) => {
+//     let me = document.createElement("h1");
+//     me.setAttribute("id", "me")
+//     let ultra = document.createTextNode("Darius Tae Freman");
+//     me.appendChild(ultra);
+//     document.body.appendChild(me);
+
+//     let lis = document.createElement("ul");
+//     lis.setAttribute("id", "lis");
+//     document.body.appendChild(lis);
+//     let listed = [
+//         "<li>Is trying to be better at this.</li>",
+//         "<li>Will work harder... promise.</li>",
+//         "<li>Is Thankful for the friends who support me.</li>",
+//         "<li>Will give this everything I can give.</li>",
+//         "<li>Will Not Fail.</li>"
+//     ]
+//     let listItself = listed.join(" ");
+//      const listings = document.getElementById("lis");
+//      listings.innerHTML = listItself;
+// }
+//     const button = document.getElementById('increment-count');
+//     const count = document.getElementById("clicked-count");
+//     button.addEventListener('click', (event) => {
+//         count.innerHTML = `${event.detail}`;
+//     })
+// })
+
